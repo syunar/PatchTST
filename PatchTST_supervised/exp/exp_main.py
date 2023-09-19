b103e7cfd9f8c44989cf803331d7f8a14e066404
Fronk Supakorn <109573035+syunar@users.noreply.github.com>
2023-09-19 09:25:33 +0000

    debug

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -316,55 +316,56 @@ class Exp_Main(Exp_Basic):
     def predict(self, setting, load=False):
         pred_data, pred_loader = self._get_data(flag='pred')
 
-        ipdb.set_trace()
+        return pred_data, pred_loader
+        
 
-        if load:
-            path = os.path.join(self.args.checkpoints, setting)
-            best_model_path = path + '/' + 'checkpoint.pth'
-            self.model.load_state_dict(torch.load(best_model_path))
+        # if load:
+        #     path = os.path.join(self.args.checkpoints, setting)
+        #     best_model_path = path + '/' + 'checkpoint.pth'
+        #     self.model.load_state_dict(torch.load(best_model_path))
 
-        preds = []
+        # preds = []
 
-        self.model.eval()
-        with torch.no_grad():
-            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
-                batch_x = batch_x.float().to(self.device)
-                batch_y = batch_y.float()
-                batch_x_mark = batch_x_mark.float().to(self.device)
-                batch_y_mark = batch_y_mark.float().to(self.device)
+        # self.model.eval()
+        # with torch.no_grad():
+        #     for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
+        #         batch_x = batch_x.float().to(self.device)
+        #         batch_y = batch_y.float()
+        #         batch_x_mark = batch_x_mark.float().to(self.device)
+        #         batch_y_mark = batch_y_mark.float().to(self.device)
 
-                # decoder input
-                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(batch_y.device)
-                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-                # encoder - decoder
-                if self.args.use_amp:
-                    with torch.cuda.amp.autocast():
-                        if 'Linear' in self.args.model or 'TST' in self.args.model:
-                            outputs = self.model(batch_x)
-                        else:
-                            if self.args.output_attention:
-                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                            else:
-                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                else:
-                    if 'Linear' in self.args.model or 'TST' in self.args.model:
-                        outputs = self.model(batch_x)
-                    else:
-                        if self.args.output_attention:
-                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                        else:
-                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                pred = outputs.detach().cpu().numpy()  # .squeeze()
-                preds.append(pred)
+        #         # decoder input
+        #         dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(batch_y.device)
+        #         dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+        #         # encoder - decoder
+        #         if self.args.use_amp:
+        #             with torch.cuda.amp.autocast():
+        #                 if 'Linear' in self.args.model or 'TST' in self.args.model:
+        #                     outputs = self.model(batch_x)
+        #                 else:
+        #                     if self.args.output_attention:
+        #                         outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+        #                     else:
+        #                         outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+        #         else:
+        #             if 'Linear' in self.args.model or 'TST' in self.args.model:
+        #                 outputs = self.model(batch_x)
+        #             else:
+        #                 if self.args.output_attention:
+        #                     outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+        #                 else:
+        #                     outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+        #         pred = outputs.detach().cpu().numpy()  # .squeeze()
+        #         preds.append(pred)
 
-        preds = np.array(preds)
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        # preds = np.array(preds)
+        # preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
 
-        # result save
-        folder_path = './results/' + setting + '/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+        # # result save
+        # folder_path = './results/' + setting + '/'
+        # if not os.path.exists(folder_path):
+        #     os.makedirs(folder_path)
 
-        np.save(folder_path + 'real_prediction.npy', preds)
+        # np.save(folder_path + 'real_prediction.npy', preds)
 
         return
